454bcd3f7676668338c683f61569f7b25329acdb
Sanjeetkumarrao <sanjeetkumarbharti2018@gmail.com>
2025-09-07 19:23:57 +0530

    feat: add test route to verify server functionality

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+app.get('/test', (req, res) => {
+  res.send('Another route is testing!');
+});
 
 app.get("/api", (req, res) => {
   res.json({ message: "API is working!" });
