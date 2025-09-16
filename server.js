585290b20a1a4cd83c9520e8831969577c727c82
learnonix <surya01785@gmail.com>
2025-09-17 04:25:02 +0530

    server.js

--- server.js
+++ server.js
@@ -13,14 +13,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
 connectDB();
 
-// Routes
 app.use("/api/heritages", heritageRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
-app.use("/api/ai", aiRoutes);
+app.use("/api", aiRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
